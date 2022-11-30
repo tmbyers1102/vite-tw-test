2d241914f2cd716c404d3f81ccc6d1ee7b1ac027
Tom Byers <tmbyers1102@gmail.com>
2022-11-30 08:18:22 -0800

    vite tw initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-amber-600 to-indigo-700">
+      <div className="text-white text-xl text-bold flex items-center justify-center">
+        <h2 className="m-6">I am item</h2>
+      </div>
+
+    </div>    
+  )
+}
+
+export default App
